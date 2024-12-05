6f9bbc1405db552aefe6c53e3cc1d0dcc6aa86bd
BUBUSTEIN <bubustein.official@gmail.com>
2024-12-05 13:24:42 +0200

    Update ModBlocks.java

--- common/src/main/java/tk/bubustein/money/block/ModBlocks.java
+++ common/src/main/java/tk/bubustein/money/block/ModBlocks.java
@@ -10,7 +10,7 @@ import tk.bubustein.money.MoneyMod;
 import tk.bubustein.money.block.custom.ATM;
 import tk.bubustein.money.block.custom.BankMachine;
 import java.util.function.Supplier;
-
+@SuppressWarnings("UnstableApiUsage")
 public class ModBlocks {
     public static void init(){};
     public static final Supplier<Block> ATM = registerBlock("atm",
